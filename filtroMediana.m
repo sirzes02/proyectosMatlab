8d00f18d8179dae418984c2e7aa8b0bf4e6c583e
S_Varela <33233964+sirzes02@users.noreply.github.com>
2020-04-23 12:05:32 -0500

    cambios

--- filtroMediana.m
+++ filtroMediana.m
@@ -1,5 +1,4 @@
-%autores: Catalina mendoza - 1193578869
-%         Santiago Varela - 1005868161
+%autores: Catalina mendoza - 1193578869, Santiago Varela - 1005868161
 
 % Declaración de función con retorno de imagen  y parametros de imagen,
 % dimensiones, r y c
@@ -11,17 +10,17 @@ disp("Procesando filtro mediana...");
 prom = (r:c);
 
 % Recorrido de filas
-for x = 2:r - dimensiones
+for x = dimensiones + 1:r - dimensiones
     % Recorrido de columnas
-    for y = 2:c - dimensiones
+    for y = dimensiones + 1:c - dimensiones
         % Definición y reinicio de a como vector
         a = [0; 0];
         % Definición y reinicio de t como 0
         t = 1;
         % Recorrido de las filas externas al pixel
-        for u = -1: dimensiones
+        for u = -dimensiones: dimensiones
             % Recorrido de las columnas externas al pixel
-            for v = -1: dimensiones
+            for v = -dimensiones: dimensiones
                 % Guardado de los puntos al rededor del pixel (x y)
                 a(t) = imagen(x + u, y + v);
                 % Aumento en t de 1
@@ -31,13 +30,11 @@ for x = 2:r - dimensiones
         % Ordenado del vecto a
         a1 = sort(a);
         
-        % Se resta 1 a t para quitarle la propiedad de indexador y
-        % convertilo en contador
-        t = t - 1;
-        
-        % Se obtiene la parte entera de la mitad del largo de a y se toma
-        % ese valor como indexador para el vector a1
-        med = a1(fix(t/2));
+        if mod(t, 2) == 0
+            med = a1(fix(t/2));
+        else
+            med = a1(fix(t/2) + 1);
+        end
         
         % Se le da el valor de la mediana a la posición del pixel de la
         % imagen
--- filtroPromedio.m
+++ filtroPromedio.m
@@ -1,5 +1,4 @@
-%autores: Catalina mendoza - 1193578869
-%         Santiago Varela - 1005868161
+%autores: Catalina mendoza - 1193578869, Santiago Varela - 1005868161
 
 % Declaración de función con retorno de imagen  y parametros de imagen,
 % dimensiones, r y c
@@ -11,17 +10,17 @@ disp("Procesando filtro promedio...");
 prom = [0;0];
 
 % Recorrido de filas
-for i = 2:r - dimensiones
+for i = dimensiones + 1:r - dimensiones
     % Recorrido de columnas
-    for j = 2:c - dimensiones
+    for j = dimensiones + 1:c - dimensiones
         % Definición y reinicio de a como 0
         a = 0;
         % Definición y reinicio de t como 0
         t = 0;
         % Recorrido de las filas externas al pixel
-        for u = -1: dimensiones
+        for u = -dimensiones: dimensiones
             % Recorrido de las columnas externas al pixel
-            for v = -1: dimensiones
+            for v = -dimensiones: dimensiones
                 % Acumulador de suma de cada pixel externo al pixel (i j)
                 a = a + imagen(i + u, j + v);
                 % Aumento en t de 1
